8dbe4b9b42ee78fdd44a5bd36abbb4cffb65d360
smadhan0 <70846824+smadhan0@users.noreply.github.com>
2022-11-02 09:46:25 -0400

    Update variable.tf
    Updating the new keys

--- Meta Arguments/variable.tf
+++ Meta Arguments/variable.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
-  default     = "AKIAWI4DOLHUOXYNF7FW"
+  default     = "AKIAWI4DOLHUOAXKXM5J"
   description = "AWS Access Key"
   sensitive=true
 }
 
 variable "aws_secret_key" {
-  default     = "lIkj0uxWRrQZIDWzdKS53R83WJy3n6gBTPyzE3NB"
+  default     = "6EDzGSIA4u5tx96DnY088v3mWAwVkty3E+19PqCD"
   description = "AWS Secret Key"
   sensitive=true
 }
