8e40e13974176ba40ee7d95979e36646e597f62d
smadhan0 <70846824+smadhan0@users.noreply.github.com>
2022-11-02 09:35:38 -0400

    Create variable.tf

---
+++ Meta Arguments/variable.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+  default     = "AKIAWI4DOLHUOXYNF7FW"
+  description = "AWS Access Key"
+  sensitive=true
+}
+
+variable "aws_secret_key" {
+  default     = "lIkj0uxWRrQZIDWzdKS53R83WJy3n6gBTPyzE3NB"
+  description = "AWS Secret Key"
+  sensitive=true
+}
+
