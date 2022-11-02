516efe99e595b4e9cda29f2c75e1967be5c9479c
smadhan0 <70846824+smadhan0@users.noreply.github.com>
2022-11-02 09:58:03 -0400

    Update main.tf

--- Meta Arguments/main.tf
+++ Meta Arguments/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
 region = "us-east-2"
-access_key = var.aws_access_kay
-secret_key = var.aws_secret_key
 }
 resource "aws_instance" "count_terraform_test" {
 # create four similar EC2 instances
